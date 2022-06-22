e76e988f22e8b210b44f749e1028803785d86cc7
justjapann <eduardobessadesouza@gmail.com>
2022-06-22 12:08:09 -0300

    base ok

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,176 @@
+use ansi_term::Colour;
 use rand::Rng;
 use std::io;
-use std::{thread, time::Duration};
+use std::{thread, time};
 
-fn answer(input_string: &str) {
-    let mut input_string = String::new();
+fn answer() -> String {
+    let mut input_user = String::new();
     io::stdin()
-        .read_line(&mut input_string)
+        .read_line(&mut input_user)
         .expect("Failed to read line.");
-    let input_string = input_string.trim_end();
+    let input_user = input_user.trim_end();
+
+    return input_user.to_string();
 }
 
 fn main() {
-    let mut input_string = String::new();
-    io::stdin()
-        .read_line(&mut input_string)
-        .expect("Failed to read line.");
-    let input_string = input_string.trim_end();
-    let pergunta1 = answer(input_string);
-
+    let ten_millis = time::Duration::from_millis(1000);
     let mut rng = rand::thread_rng();
 
-    let player: u8 = rng.gen_range(0..10);
+    let player: u8 = rng.gen_range(1..10);
 
-    let dealer: u8 = rng.gen_range(0..10);
+    let dealer: u8 = rng.gen_range(1..10);
 
-    println!("you {}\ndealer {}\nHit or Stand?", player, dealer);
+    println!("You {}", Colour::Blue.paint(player.to_string()),);
+    thread::sleep(ten_millis);
+    println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+    thread::sleep(ten_millis);
+    println!("Hit or Stand?");
+    let answer_user = answer();
 
-    if pergunta1 == "hit" {
-        let player: u8 = player + rng.gen_range(0..10);
-        let dealer: u8 = dealer + rng.gen_range(0..10);
+    if answer_user == "hit" {
+        let player: u8 = player + rng.gen_range(1..10);
+        let dealer: u8 = dealer + rng.gen_range(1..10);
 
-        println!("You {}\nDealer {}\nHit or Stand?", player, dealer);
+        println!("--------------------------------");
+        println!("You {}", Colour::Blue.paint(player.to_string()),);
+        thread::sleep(ten_millis);
+        println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+        thread::sleep(ten_millis);
+        println!("Hit or Stand?");
+        let answer_user = answer();
+
+        if answer_user == "hit" {
+            let player: u8 = player + rng.gen_range(1..10);
+            let dealer: u8 = dealer + rng.gen_range(1..10);
+            println!("--------------------------------");
+            println!("You {}", Colour::Blue.paint(player.to_string()),);
+            println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+            thread::sleep(ten_millis);
+            let dealer: u8 = dealer + rng.gen_range(1..10);
+            println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+            thread::sleep(ten_millis);
+
+            if player > 21 {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Red.paint("You lost"),
+                    player,
+                    dealer
+                );
+            } else if dealer > 21 {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Green.paint("You win"),
+                    player,
+                    dealer
+                );
+            } else if player > dealer {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Green.paint("You win"),
+                    player,
+                    dealer
+                );
+            } else {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Red.paint("You lost"),
+                    player,
+                    dealer
+                );
+            }
+        } else if answer_user == "stand" {
+            let dealer: u8 = dealer + rng.gen_range(1..10);
+            println!("--------------------------------");
+            println!("You {}", Colour::Blue.paint(player.to_string()),);
+            thread::sleep(ten_millis);
+            println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+            thread::sleep(ten_millis);
+
+            if dealer > 21 {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Green.paint("You win"),
+                    player,
+                    dealer
+                );
+            } else if player > dealer {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Green.paint("You win"),
+                    player,
+                    dealer
+                );
+            } else {
+                println!("--------------------------------");
+                println!(
+                    "{}!!\nYou {}\nDealer {}\n",
+                    Colour::Red.paint("You lost"),
+                    player,
+                    dealer
+                );
+            }
+        } else if player > dealer {
+            println!("--------------------------------");
+            println!(
+                "{}!!\nYou {}\nDealer {}\n",
+                Colour::Green.paint("You win"),
+                player,
+                dealer
+            );
+        } else {
+            println!("--------------------------------");
+            println!(
+                "{}!!\nYou {}\nDealer {}\n",
+                Colour::Red.paint("You lost"),
+                player,
+                dealer
+            );
+        }
+    } else if answer_user == "stand" {
+        let dealer: u8 = dealer + rng.gen_range(1..10);
+        println!("--------------------------------");
+        println!("You {}", Colour::Blue.paint(player.to_string()),);
+        thread::sleep(ten_millis);
+        println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+        thread::sleep(ten_millis);
+        let dealer: u8 = dealer + rng.gen_range(1..10);
+        println!("Dealer {}", Colour::Yellow.paint(dealer.to_string()),);
+        thread::sleep(ten_millis);
+
+        if dealer > 21 {
+            println!("--------------------------------");
+            println!(
+                "{}!!\nYou {}\nDealer {}\n",
+                Colour::Green.paint("You win"),
+                player,
+                dealer
+            );
+        } else if player > dealer {
+            println!("--------------------------------");
+            println!(
+                "{}!!\nYou {}\nDealer {}\n",
+                Colour::Green.paint("You win"),
+                player,
+                dealer
+            );
+        } else {
+            println!("--------------------------------");
+            println!(
+                "{}!!\nYou {}\nDealer {}\n",
+                Colour::Red.paint("You lost"),
+                player,
+                dealer
+            );
+        }
     } else {
+        println!("Please, choose some option");
     }
 }
