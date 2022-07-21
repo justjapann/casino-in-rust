83e51c895a3004233ed0822244ec093ee0fac32c
justjapann <eduardobessadesouza@gmail.com>
2022-07-20 23:46:18 -0300

    added the first cards

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #[path = "./blackjack/blackjack.rs"]
 mod blackjack;
+#[path = "./blackjack/cards/cards.rs"]
+mod cards;
 mod data;
 
 fn main() {
-    blackjack::main();
+    //blackjack::main();
+    cards::oneAs();
+    cards::fourAs()
 }
